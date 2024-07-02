ab4fc741e2a3388e814adc99d5bf7829eca07e27
gr00ze <vincenzop23@gmail.com>
2024-07-03 00:44:02 +0200

    Now they actually spawn

--- src/main/java/com/jamieswhiteshirt/trumpetskeleton/TrumpetSkeleton.java
+++ src/main/java/com/jamieswhiteshirt/trumpetskeleton/TrumpetSkeleton.java
@@ -1,5 +1,6 @@
 package com.jamieswhiteshirt.trumpetskeleton;
 
+import com.jamieswhiteshirt.trumpetskeleton.common.entity.TrumpetSkeletonWorldSpawner;
 import com.jamieswhiteshirt.trumpetskeleton.common.entity.TrumpetSkeletonEntityTypes;
 import com.jamieswhiteshirt.trumpetskeleton.common.item.TrumpetSkeletonItems;
 import com.jamieswhiteshirt.trumpetskeleton.common.sound.TrumpetSkeletonSoundEvents;
@@ -22,14 +23,17 @@ import java.util.Properties;
 
 public class TrumpetSkeleton implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger("trumpet-skeleton");
+    public static final String MOD_ID = "trumpet-skeleton";
 
-    public static double relativeSpawnWeight = 0.05D;
+    public static double relativeSpawnWeight = 100D; //0.05D
 
     @Override
     public void onInitialize() {
+
         TrumpetSkeletonItems.init();
         TrumpetSkeletonSoundEvents.init();
         TrumpetSkeletonEntityTypes.init();
+        TrumpetSkeletonWorldSpawner.init();
 
         ParrotEntityAccessor.trumpetskeleton$getMobSounds().put(TrumpetSkeletonEntityTypes.TRUMPET_SKELETON, TrumpetSkeletonSoundEvents.ENTITY_PARROT_IMITATE_TRUMPET_SKELETON);
         SpawnRestrictionAccessor.trumpetskeleton$register(TrumpetSkeletonEntityTypes.TRUMPET_SKELETON, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, HostileEntity::canSpawnInDark);
--- src/main/java/com/jamieswhiteshirt/trumpetskeleton/common/entity/TrumpetSkeletonEntityTypes.java
+++ src/main/java/com/jamieswhiteshirt/trumpetskeleton/common/entity/TrumpetSkeletonEntityTypes.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.SpawnGroup;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+import static com.jamieswhiteshirt.trumpetskeleton.TrumpetSkeleton.MOD_ID;
 import static net.minecraft.registry.Registries.ENTITY_TYPE;
 
 
@@ -16,7 +17,7 @@ public class TrumpetSkeletonEntityTypes {
     public static final EntityType<TrumpetSkeletonEntity> TRUMPET_SKELETON = register("trumpet_skeleton", FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, TrumpetSkeletonEntity::new).dimensions(EntityDimensions.changing(0.6F, 1.99F)));
 
     private static <T extends Entity> EntityType<T> register(String id, FabricEntityTypeBuilder<T> builder) {
-        return register(new Identifier("trumpet-skeleton", id), builder);
+        return register(new Identifier(MOD_ID, id), builder);
     }
 
     private static <T extends Entity> EntityType<T> register(Identifier id, FabricEntityTypeBuilder<T> builder) {
