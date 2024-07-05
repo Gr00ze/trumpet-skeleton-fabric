35263f0244eddbad84d28705f1a9cb8b8451c2ac
Gr00ze <vincenzop23@gmail.com>
2024-07-05 17:01:44 +0200

    Restored trumpet item settings

--- src/main/java/com/jamieswhiteshirt/trumpetskeleton/common/item/TrumpetSkeletonItems.java
+++ src/main/java/com/jamieswhiteshirt/trumpetskeleton/common/item/TrumpetSkeletonItems.java
@@ -10,15 +10,18 @@ import net.minecraft.util.Identifier;
 
 
 public class TrumpetSkeletonItems {
-    public static final Item TRUMPET = register("trumpet", new TrumpetItem(new FabricItemSettings()));
+    public static final Item TRUMPET = register("trumpet", new TrumpetItem(new FabricItemSettings().maxCount(1).maxDamage(200)));
 
 
     public static final Item TRUMPET_SKELETON_SPAWN_EGG = register("trumpet_skeleton_spawn_egg", new SpawnEggItem(TrumpetSkeletonEntityTypes.TRUMPET_SKELETON, 0xC1C1C1, 0xFCFC00, (new Item.Settings())));
 
     private static Item register(String id, Item item) {
+        /*
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(content -> {
             content.add(TRUMPET);
         });
+        */
+
         return register(new Identifier("trumpet-skeleton", id), item);
     }
 
@@ -30,5 +33,12 @@ public class TrumpetSkeletonItems {
         return Registry.register(Registries.ITEM, id, item);
     }
 
-    public static void init() { }
+    public static void init() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(content -> {
+            content.add(TRUMPET);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> {
+            content.add(TRUMPET_SKELETON_SPAWN_EGG);
+        });
+    }
 }
