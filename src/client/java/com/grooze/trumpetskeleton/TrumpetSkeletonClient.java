dcb188801c8932f454e14ee3a61c4a8f74d074c6
gr00ze <vincenzop23@gmail.com>
2024-07-06 21:54:55 +0200

    Added template

---
+++ src/client/java/com/grooze/trumpetskeleton/TrumpetSkeletonClient.java
@@ -0,0 +1,10 @@
+package com.grooze.trumpetskeleton;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrumpetSkeletonClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
