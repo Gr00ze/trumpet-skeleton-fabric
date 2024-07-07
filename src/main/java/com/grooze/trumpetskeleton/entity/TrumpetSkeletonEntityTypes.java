93d41b3b3bf9336d055f8316a434a9ce70d747a7
gr00ze <vincenzop23@gmail.com>
2024-07-07 16:39:13 +0200

    Updated to new functions: -FabricEntityTypeBuilder is now EntityType.Builder.create and it accept the same swapped parameters -new Identifier is now Identifier.of -dimension(EntityDimension(float, float)) is now dimension(float,float)

--- src/main/java/com/grooze/trumpetskeleton/entity/TrumpetSkeletonEntityTypes.java
+++ src/main/java/com/grooze/trumpetskeleton/entity/TrumpetSkeletonEntityTypes.java
@@ -1,8 +1,7 @@
 package com.grooze.trumpetskeleton.entity;
 
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.registry.Registry;
