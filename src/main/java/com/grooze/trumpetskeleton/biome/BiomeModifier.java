f2f0f89b9119998f7073d4611791eede3ee01dc4
gr00ze <vincenzop23@gmail.com>
2024-07-07 17:55:11 +0200

    Update 1.21 Same fix 1.20.1

---
+++ src/main/java/com/grooze/trumpetskeleton/biome/BiomeModifier.java
@@ -0,0 +1,23 @@
+package com.grooze.trumpetskeleton.biome;
+
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.entity.SpawnGroup;
+
+import static com.grooze.trumpetskeleton.TrumpetSkeleton.relativeSpawnWeight;
+import static com.grooze.trumpetskeleton.entity.TrumpetSkeletonEntityTypes.TRUMPET_SKELETON;
+
+public class BiomeModifier {
+
+    public static void init(){
+        //this fix spawn skeleton with same weight everywhere
+        BiomeModifications.addSpawn(
+                BiomeSelectors.all(),
+                SpawnGroup.MONSTER,
+                TRUMPET_SKELETON,
+                (int)Math.ceil(100 * relativeSpawnWeight),
+                1,
+                1
+        );
+    }
+}
