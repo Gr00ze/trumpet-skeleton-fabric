3f8310500e018b766e6debcbeefc82af2c40a417
gr00ze <vincenzop23@gmail.com>
2024-07-07 17:53:46 +0200

    Update 1.21 Client

--- src/client/java/com/grooze/trumpetskeleton/TrumpetSkeletonClient.java
+++ src/client/java/com/grooze/trumpetskeleton/TrumpetSkeletonClient.java
@@ -1,10 +1,13 @@
 package com.grooze.trumpetskeleton;
 
+import com.grooze.trumpetskeleton.entity.TrumpetSkeletonEntityTypes;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.render.entity.SkeletonEntityRenderer;
 
 public class TrumpetSkeletonClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		EntityRendererRegistry.register(TrumpetSkeletonEntityTypes.TRUMPET_SKELETON, SkeletonEntityRenderer::new);
 	}
 }
